deae1bfe21c6ddb20532082223142d316c427ec7
Patrick <hansenp2@tcnj.edu>
2019-06-03 18:00:09 -0400

    adding files to repo for deployment

---
+++ precache-manifest.a0d306702bbb5978d71bef3f103e0f2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8391109e222dd22d01aa213ccf4269f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ad4c82acbb5233e6e7b",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5ad4c82acbb5233e6e7b",
+    "url": "/static/js/main.6a10a90d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
