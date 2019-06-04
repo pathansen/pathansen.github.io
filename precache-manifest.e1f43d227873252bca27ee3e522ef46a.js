b20738e6a47a04deaca8034c36773507017718d3
Patrick <hansenp2@tcnj.edu>
2019-06-04 17:12:47 -0400

    site with styling

---
+++ precache-manifest.e1f43d227873252bca27ee3e522ef46a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868e118963ee7cab3d9dc624e349b8b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e06b1458c694818bb663",
+    "url": "/static/css/main.fe3078b2.chunk.css"
+  },
+  {
+    "revision": "15de58e2ed34d1cb0721",
+    "url": "/static/js/2.cdccf80e.chunk.js"
+  },
+  {
+    "revision": "e06b1458c694818bb663",
+    "url": "/static/js/main.cccf2f84.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a0010381fbd6c6a0de6d97c452ac72f",
+    "url": "/static/media/cloud.2a001038.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b3b05a7c8cac8a27b37568f12642f2a.js"
+  "/precache-manifest.e1f43d227873252bca27ee3e522ef46a.js"
 );
 
 self.addEventListener('message', (event) => {
